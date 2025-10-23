bd280fa13de61560f92efe626ff25f9144a2bc94
Viktar Salauyou <night_elf@tut.by>
2025-10-23 19:18:24 +0300

    Add background image

---
+++ app/src/main/java/com/example/myapplication2/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication2.databinding.ActivitySecondBinding
+
+class SecondActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
+    }
+}
